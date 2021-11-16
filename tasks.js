5a6f7241f183b9e538abd2b0e0965299391622bd
Steven Kristjan <s.rekand@khk.ee>
2021-11-17 01:17:57 +0200

    Task delete button

--- tasks.js
+++ tasks.js
@@ -1,7 +1,9 @@
 const taskInput = document.querySelector('#task')
 const form = document.querySelector('form')
+const tasksList = document.querySelector('.collection')
 
 form.addEventListener('submit', addTask)
+tasksList.addEventListener('click', deleteTask)
 
 function addTask(e){
     const task = taskInput.value
@@ -11,9 +13,28 @@ function addTask(e){
     const text = document.createTextNode(task)
     li.appendChild(text)
 
+// create link element
+    const link = document.createElement('a')
+
+    link.setAttribute('href', '#')
+    link.className = 'secondary-content'
+
+    link.appendChild(document.createTextNode('X'))
+    li.appendChild(link)
+
+    console.log(link)
+
     const ul = document.querySelector('.collection')
     ul.appendChild(li)
 
+// value is 0
     taskInput.value = ''
     e.preventDefault()
+}
+
+function deleteTask(e){
+    if(e.target.textContent == 'X'){
+        if(confirm('Do you want to delete this task?'))
+            e.target.parentElement.remove()
+    }
 }
